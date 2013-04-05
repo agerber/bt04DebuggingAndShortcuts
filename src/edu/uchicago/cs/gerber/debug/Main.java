def12b8012fa0320c5999736a2cfe5cca53138fd
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 11:54:09 -0500

    p1

--- src/edu/uchicago/cs/gerber/debug/Main.java
+++ src/edu/uchicago/cs/gerber/debug/Main.java
@@ -4,13 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Main extends Activity   {
+public class Main extends Activity implements OnClickListener   {
     /** Called when the activity is first created. */
 	
+	// %%%%%%%%%%%%%% PHASE 1
+	TextView txtShow;
+	Button btnGo;
 
 	
 	// %%%%%%%%%%%%%% PHASE 0
@@ -19,11 +21,26 @@ public class Main extends Activity   {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-
+        // %%%%%%%%%%%%%% PHASE 1
+       	//set some breakpoints here in this method
+           txtShow = (TextView) findViewById(R.id.txtShow);
+           btnGo = (Button) findViewById(R.id.btnGo);
+           
+           //let this class implement the OnClickListener
+           btnGo.setOnClickListener(this);
 
     }
 
 
+    // %%%%%%%%%%%%%% PHASE 1
+   	@Override
+   	public void onClick(View v) {
+
+   		//String strBuild = getStringArray();
+   		//txtShow.setText(strBuild);
+   		
+   		
+   	}
 
 
 

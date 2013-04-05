e774b95caa560b226c753d50e8db8dec74c2f704
Adam Gerber <adam.gerber@yahoo.com>
2013-04-05 11:51:47 -0500

    p0

--- src/edu/uchicago/cs/gerber/debug/Main.java
+++ src/edu/uchicago/cs/gerber/debug/Main.java
@@ -8,76 +8,23 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class Main extends Activity implements OnClickListener  {
+public class Main extends Activity   {
     /** Called when the activity is first created. */
 	
-	TextView txtShow;
-	Button btnGo;
+
 	
-	
+	// %%%%%%%%%%%%%% PHASE 0
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-    	//set some breakpoints here in this method
-        txtShow = (TextView) findViewById(R.id.txtShow);
-        btnGo = (Button) findViewById(R.id.btnGo);
         
-        //let this class implement the OnClickListener
-        btnGo.setOnClickListener(this);
+
 
     }
 
 
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub
-		
-		//R.array.hello_array
-		String strBuild = getStringArray();
-		txtShow.setText(strBuild);
-		
-		
-	}
 
 
-	private String getStringArray() {
-		//set some breakpoints here in this method
-		String[] strWords = getResources().getStringArray(R.array.hello_array);
-		
-		String strBuild = "";
-		for (String str : strWords) {
-			
-			strBuild += str + " ";
-		}
-		//set a breakpoint at line below 
-		strBuild += "\n" + convertToAscii(strBuild);
-		return strBuild;
-	}
-	
-	private String convertToAscii(String strSentence){
-		
-	   //declare some vars
-	   String strR = "";
-	   char cChar;
-	   int nCode;
-	   
-	   //iterate over the string
-		for (int nC = 0; nC < strSentence.length()-1; nC++) {
-			cChar = strSentence.charAt(nC);
-			nCode = (int) cChar;
-		    strR += nCode + ":";
-		}
-		
-		//convert the last character
-		cChar = strSentence.charAt(strSentence.length()-1);
-		nCode = (int) cChar;
-		strR += nCode;
-
-		//return the built string
-		return strR;
-		
-	
-	}
 
 }
